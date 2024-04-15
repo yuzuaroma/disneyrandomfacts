e84b67c82328857acb90d41552e752a24d1616ca
yuzuaroma <yuzuka.raphaela@gmail.com>
2024-04-15 17:34:55 +0900

    自由にわかりやすいメッセージ

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,22 @@
- class CommentsController < ApplicationController
-    before_action :authenticate_user!
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
   
-    def create
-      yuzu = Yuzu.find(params[:yuzu_id])
-      comment = yuzu.comments.build(comment_params)
-      comment.user_id = current_user.id
-      if comment.save
-        flash[:success] = "コメントしました"
-        redirect_back(fallback_location: root_path)
-      else
-        flash[:error] = "コメントできませんでした"
-        redirect_back(fallback_location: root_path) 
-      end
-    end
-  
-    private
-  
-    def comment_params
-      params.require(:comment).permit(:content)
+  def create
+    yuzu = Yuzu.find(params[:yuzu_id])
+    comment = yuzu.comments.build(comment_params)
+    comment.user_id = current_user.id
+    if comment.save
+      flash[:success] = "コメントしました"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:error] = "コメントできませんでした"
+      redirect_back(fallback_location: root_path) 
     end
   end
+
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+end
